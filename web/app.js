2cae3638e236151afa17bb8958d802720aaa934e
KpryKorn <roffinisacha@gmail.com>
2023-10-03 14:18:00 +0200

    simplified code w/ copilot chat

--- web/app.js
+++ web/app.js
@@ -1,46 +1,21 @@
-/*
- * Copyright 2022 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 require("dotenv").config();
 
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const { GoogleAuth } = require("google-auth-library");
 const jwt = require("jsonwebtoken");
 
-// TODO: Define Issuer ID
 const issuerId = "3388000000022272047";
-
-// TODO: Define Class ID
 const classId = `${issuerId}.test_class`;
-
 const baseUrl = "https://walletobjects.googleapis.com/walletobjects/v1";
-
 const credentials = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
 const httpClient = new GoogleAuth({
   credentials: credentials,
   scopes: "https://www.googleapis.com/auth/wallet_object.issuer",
 });
 
 async function createPassClass(req, res) {
-  // TODO: Create a Generic pass class
-
-  let genericClass = {
+  const genericClass = {
     id: `${classId}`,
     classTemplateInfo: {
       cardTemplateOverride: {
@@ -103,41 +78,34 @@ async function createPassClass(req, res) {
     },
   };
 
-  let response;
   try {
-    // Check if the class exists already
-    response = await httpClient.request({
+    await httpClient.request({
       url: `${baseUrl}/genericClass/${classId}`,
       method: "GET",
     });
-
     console.log("Class already exists");
   } catch (err) {
     if (err.response && err.response.status === 404) {
-      // Class does not exist
-      // Create it now
-      response = await httpClient.request({
+      const response = await httpClient.request({
         url: `${baseUrl}/genericClass`,
         method: "POST",
         data: genericClass,
       });
-
       console.log("Class insert response");
       console.log(response);
     } else {
-      // Something else went wrong
       console.log(err);
       res.send("Something went wrong...check the console logs!");
+      return;
     }
   }
 }
 
 async function createPassObject(req, res) {
-  // TODO: Create a new Generic pass for the user
-  let objectSuffix = `${req.body.email.replace(/[^\w.-]/g, "_")}`;
-  let objectId = `${issuerId}.${objectSuffix}`;
+  const objectSuffix = `${req.body.email.replace(/[^\w.-]/g, "_")}`;
+  const objectId = `${issuerId}.${objectSuffix}`;
 
-  let genericObject = {
+  const genericObject = {
     id: `${objectId}`,
     classId: classId,
     genericType: "GENERIC_TYPE_UNSPECIFIED",
@@ -259,7 +227,6 @@ async function createPassObject(req, res) {
     hexBackgroundColor: "#6AA517",
   };
 
-  // TODO: Create the signed JWT and link
   const claims = {
     iss: credentials.client_email,
     aud: "google",
@@ -270,7 +237,6 @@ async function createPassObject(req, res) {
     },
   };
 
-  // FIX : le wallet généré dépend de l'adresse mail (et donc de l'ID) unique ??
   const token = jwt.sign(claims, credentials.private_key, {
     algorithm: "RS256",
   });
