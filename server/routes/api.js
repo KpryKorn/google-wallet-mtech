2e52b5df98937f42c2d4a0bc4cb99d061364ed3b
KpryKorn <roffinisacha@gmail.com>
2023-10-11 11:47:29 +0200

    feat: add api route

---
+++ server/routes/api.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+// renvoie les données spécifiques à iOS sous "/api/apple"
+router.get("/apple", (req, res, next) => {
+  try {
+    const filePath = path.join(
+      __dirname,
+      "../iOS/carteAdherent.pass/pass.json"
+    );
+    res.sendFile(filePath);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// renvoie les données spécifiques à android sous "/api/android"
+router.get("/android", (req, res, next) => {
+  try {
+    res.send("Android");
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
