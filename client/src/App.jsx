20837f356fecf2587441a1464094d6ab002cfa55
KpryKorn <roffinisacha@gmail.com>
2023-10-11 13:08:04 +0200

    feat: add GET request to Apple Data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,28 @@ import DesktopView from "./components/Views/DesktopView";
 import AppleView from "./components/Views/AppleView";
 import AndroidView from "./components/Views/AndroidView";
 import { isUserAgentMobile, isUserAgentIOS } from "./lib/utils";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [appleData, setAppleData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/apple")
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        setAppleData(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   const isMobile = isUserAgentMobile();
   const isIOS = isUserAgentIOS();
 
-  return isMobile ? isIOS ? <AppleView /> : <AndroidView /> : <DesktopView />;
+  // return isMobile ? isIOS ? <AppleView /> : <AndroidView /> : <DesktopView />;
+  return appleData;
 }
 
 export default App;
