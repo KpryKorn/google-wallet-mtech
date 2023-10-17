a53b934d73ceec1927f6f2f635b9f3d44096c54e
KpryKorn <roffinisacha@gmail.com>
2023-10-17 10:06:22 +0200

    chore: removed formik

--- client/src/components/FormulaireAdherent/FormulaireApple.jsx
+++ client/src/components/FormulaireAdherent/FormulaireApple.jsx
@@ -1,5 +1,3 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
 import styles from "./FormulaireApple.module.css";
 
 function FormulaireApple(props) {
@@ -21,52 +19,58 @@ function FormulaireApple(props) {
     onAppleDataChange(updatedNameData);
   }
 
-  const formSchema = Yup.object().shape({
-    nom: Yup.string()
-      .min(2, "Trop court !")
-      .max(50, "Trop long !")
-      .required("Champ requis"),
-    prenom: Yup.string()
-      .min(2, "Trop court !")
-      .max(50, "Trop long !")
-      .required("Champ requis"),
-    numSecuSociale: Yup.string()
-      .min(15, "Doit faire 15 caractères")
-      .required("Champ requis"),
-  });
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch("http://localhost:3000/api/apple", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(appleData), // envoi les données modifiées
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data); // réponse du serveur (msg de confirmation)
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }
 
   return (
     <div className={styles.formContainer}>
       <h3>Mes informations</h3>
-      <Formik initialValues={appleData} validationSchema={formSchema}>
-        <Form>
-          <label htmlFor="nom">Nom</label>
-          <Field
-            id="nom"
-            name="nom"
-            value={appleData?.generic?.headerFields[0].value || ""}
-            onChange={handleNameInputChange}
-          />
-          <ErrorMessage component="span" name="nom" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nom">Nom</label>
+        <input
+          id="nom"
+          name="nom"
+          value={appleData?.generic?.headerFields[0].value || ""}
+          onChange={handleNameInputChange}
+        />
 
-          <label htmlFor="numSecuSociale">Numéro de sécurité sociale</label>
-          <Field
-            id="numSecuSociale"
-            name="numSecuSociale"
-            placeholder="(Ex.) 1 99 12 34 567 890 12"
-            onInput={(e) => {
-              if (e.target.value.length > 15) {
-                e.target.value = e.target.value.slice(0, 15);
-              }
-            }}
-            value={appleData?.generic?.secondaryFields[0].value || ""}
-            onChange={handleSecuInputChange}
-          />
-          <ErrorMessage component="span" name="numSecuSociale" />
+        <label htmlFor="numSecuSociale">Numéro de sécurité sociale</label>
+        <input
+          id="numSecuSociale"
+          name="numSecuSociale"
+          placeholder="(Ex.) 1 99 12 34 567 890 12"
+          onInput={(e) => {
+            if (e.target.value.length > 15) {
+              e.target.value = e.target.value.slice(0, 15);
+            }
+          }}
+          value={appleData?.generic?.secondaryFields[0].value || ""}
+          onChange={handleSecuInputChange}
+        />
 
-          <button type="submit">Télécharger</button>
-        </Form>
-      </Formik>
+        <button type="submit">Télécharger</button>
+      </form>
     </div>
   );
 }
